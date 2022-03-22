8438100d0607adb778a16293f5ec547cf1a43b1f
satheeshkumarkoti <satheeshkumarkoti@gmail.com>
2022-03-22 10:08:35 +0530

    zuul-gateway-services
    Zuul API Gateway
    What is Zuul?
    Zuul Server is an API Gateway application. It handles all the requests and performs the dynamic routing of microservice applications. It works as a front door for all the requests. It is also known as Edge Server.
    
    Zuul is built to enable dynamic routing, monitoring, resiliency, and security. It can also route the requests to multiple Amazon Auto Scaling Groups.
    
    For Example, /api/products are mapped to the product service and /api/user is mapped to the user service. The Zuul Server dynamically routes the requests to the respective backend application.

---
+++ zuul-gateway-services/src/test/java/com/sath/zuulgatewayservices/ZuulGatewayServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sath.zuulgatewayservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
